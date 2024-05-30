30f60abc59031263f05ae7e3219523cb08e26e69
Eric Chan <eric.chan2@unsw.edu.au>
2024-05-30 19:45:04 +1000

    Import microkitboard.zig

--- src/newsdfgen.zig
+++ src/newsdfgen.zig
@@ -3,6 +3,7 @@ const builtin = @import("builtin");
 const mod_sdf = @import("sdf.zig");
 const mod_vmm = @import("vmm.zig");
 const sddf = @import("sddf.zig");
+const mod_microkitboard = @import("microkitboard.zig");
 const dtb = @import("dtb");
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
@@ -17,35 +18,6 @@ const Irq = SystemDescription.Interrupt;
 const Channel = SystemDescription.Channel;
 
 const VirtualMachineSystem = mod_vmm.VirtualMachineSystem;
-
-const MicrokitBoard = enum {
-    qemu_arm_virt,
-    odroidc4,
-
-    pub fn fromStr(str: []const u8) !MicrokitBoard {
-        inline for (std.meta.fields(MicrokitBoard)) |field| {
-            if (std.mem.eql(u8, str, field.name)) {
-                return @enumFromInt(field.value);
-            }
-        }
-
-        return error.BoardNotFound;
-    }
-
-    pub fn arch(b: MicrokitBoard) SystemDescription.Arch {
-        return switch (b) {
-            .qemu_arm_virt, .odroidc4 => .aarch64,
-        };
-    }
-
-    pub fn printFields() void {
-        comptime var i: usize = 0;
-        const fields = @typeInfo(@This()).Enum.fields;
-        inline while (i < fields.len) : (i += 1) {
-            std.debug.print("{s}\n", .{fields[i].name});
-        }
-    }
-};
 
 var xml_out_path: []const u8 = "example.system";
 var sddf_path: []const u8 = "sddf";
